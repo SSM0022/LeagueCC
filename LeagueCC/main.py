8e9592c9cabfb19a43d5537b63c6bc9a44291249
SSM0022 <47508510+SSM0022@users.noreply.github.com>
2023-04-11 18:11:53 -0600

    Add get_activegame function to api_requests.py
    use the return value of get_summonerID to get the summoners current active game and return a list of participants

--- LeagueCC/main.py
+++ LeagueCC/main.py
@@ -1,9 +1,10 @@
-from api_requests import get_summonerID
+from api_requests import get_summonerID, get_activegame
 
 def main():
     sum_id = get_summonerID(input("Name: "))
+    participants = get_activegame(sum_id)
+    print(participants)
 
 if __name__ == "__main__":
     main()
-else:
-    print(__name__)    
+  
