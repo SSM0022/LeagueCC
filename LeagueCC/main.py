3737cfab76a7a705fedd953bd93b5a9395e8d41a
SSM0022 <47508510+SSM0022@users.noreply.github.com>
2023-04-17 22:59:46 -0600

    Add loop to output all champion CC spells in main.py
    Loop through list of champion names
    get list of champion spells
    search for CC spells
    output CC spells for each champion

--- LeagueCC/main.py
+++ LeagueCC/main.py
@@ -1,12 +1,13 @@
 from api_requests import get_summonerID, get_activegame, get_champspells
-from champions import find_champnames, find_cc
+from champions import find_champnames, find_ccspells
 
 
 def main():
     sum_id = get_summonerID(input("Name: "))
-    champ_list = (find_champnames(get_activegame(sum_id))) 
-    spells = get_champspells(champ_list[0])
-    print(find_cc(champ_list[0], spells))
+    champ_names = find_champnames(get_activegame(sum_id))
+    for champ in champ_names:
+        spells = get_champspells(champ)
+        print(find_ccspells(champ, spells))
 
 
 if __name__ == "__main__":
