14879ce5bf2604a5bdeaa015b8f8469952ee2d69
SSM0022 <47508510+SSM0022@users.noreply.github.com>
2023-04-20 10:09:09 -0600

    Add check_latest_version function to query RIOT API for latest client version
    check_latest_version is called in main before receiving any user input
    function writes last_check_time and latest_version to version.json file
    only queries RIOT API if timedelta between current time & last_check_time is >= one week
    latest_version is used by get_champspells function to query latest version of champ spells

--- LeagueCC/api_requests.py
+++ LeagueCC/api_requests.py
@@ -1,6 +1,46 @@
 import requests
 import os
 import urllib.parse
+import json
+import datetime
+
+
+def check_latest_version():
+    #  Set the file path for the version JSON file
+    file_path = os.path.join(os.path.dirname(__file__), "version.json")
+
+    # Load last_check_time from JSON file
+    try:
+        with open(file_path, "r") as f:
+            last_check_time = datetime.datetime.fromisoformat(
+                json.load(f)["last_check_time"]
+            )
+    except Exception as e:
+        print(e)
+
+    # Check if it has been more than a week since the last check
+    current_time = datetime.datetime.now(datetime.timezone.utc)
+    time_since_last_check = current_time - last_check_time
+    if time_since_last_check >= datetime.timedelta(weeks=1):
+        # Set the Riot API endpoint for retrieving the latest version
+        endpoint = "https://ddragon.leagueoflegends.com/api/versions.json"
+        
+        # Send a GET request to the endpoint
+        response = requests.get(endpoint)
+
+        # If the request was successful, get the latest_version from the response
+        if response.status_code == 200:
+            version = response.json()[0]
+
+        # Update last_check_time to be current_time
+        check_time = current_time.isoformat()
+
+        # Define dict to store new check_time & version
+        last_checked = {"last_check_time": check_time, "latest_version": version}
+
+        # Write the last_check_time & latest_version to version.json
+        with open(file_path, "w") as f:
+            json.dump(last_checked, f, indent=4)
 
 
 def get_summonerID(name):
@@ -25,7 +65,12 @@ def get_activegame(summoner_id):
 
 
 def get_champspells(champ_name):
-    url = f"http://ddragon.leagueoflegends.com/cdn/13.7.1/data/en_US/champion/{champ_name}.json"
+    try:
+        with open(os.path.join(os.path.dirname(__file__), "version.json"), "r") as f:
+            version = json.load(f)["latest_version"]
+    except FileNotFoundError:
+        print("version.json file not found")
+    url = f"https://ddragon.leagueoflegends.com/cdn/{version}/data/en_US/champion/{champ_name}.json"
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
--- LeagueCC/main.py
+++ LeagueCC/main.py
@@ -1,8 +1,9 @@
-from api_requests import get_summonerID, get_activegame, get_champspells
+from api_requests import get_summonerID, get_activegame, get_champspells, check_latest_version
 from champions import find_champnames, find_ccspells
 
 
 def main():
+    check_latest_version()
     sum_id = get_summonerID(input("Name: "))
     champ_names = find_champnames(get_activegame(sum_id))
     for champ in champ_names:
